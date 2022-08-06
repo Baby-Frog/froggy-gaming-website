8542d175ab607e4252cc9e8a3818448d493a3e9a
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-07 02:17:49 +0700

    :wrench::Fixing margin between section divider and content

--- froggy-gaming/src/HomepageLayout/sectionDivider/components/SectionDivider.js
+++ froggy-gaming/src/HomepageLayout/sectionDivider/components/SectionDivider.js
@@ -4,7 +4,9 @@ import "../assets/sectiondivider.css";
 const SectionDivider = ({ sectionContent }) => {
   return (
     <div className="section">
-      <h3 className="section-content">{sectionContent}</h3>
+      <h3 className="section-content">
+        <h3>{sectionContent}</h3>
+      </h3>
     </div>
   );
 };
