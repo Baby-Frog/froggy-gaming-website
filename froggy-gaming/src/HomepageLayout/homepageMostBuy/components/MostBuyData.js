357487520230f2dd211898da169cdf443645efc5
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-19 23:27:59 +0700

    :fire:: Start coding most buy section

--- froggy-gaming/src/HomepageLayout/homepageMostBuy/components/MostBuyData.js
+++ froggy-gaming/src/HomepageLayout/homepageMostBuy/components/MostBuyData.js
@@ -1,14 +1,19 @@
 import Product1 from "../assets/img/akkotiger.png";
 import Product2 from "../assets/img/akkohonkai.png";
+import Wireless from "../../../assets/wireless.png";
+import Bluetooth from "../../../assets/bluetooth.png";
+import Hotswap from "../../../assets/hotswap.png";
 export const MostBuyData = [
   {
     id: 1,
     img_url: Product1,
     product_name: "Akko PC75B Plus Year Of Tiger",
-  },
-  {
-    id: 2,
-    img_url: Product2,
-    product_name: "Akko PC75B Plus Year Of TigerAKKO 5108S Honkai Impact 3rd",
+    product_compatible: [Wireless, Hotswap, Bluetooth],
+    brand: "Akko",
+    keycap: "Bla Blo Keycap",
+    switch: "Bla Blo Switch",
+    layout: "85% TKL",
+    weight: "1 KG",
+    feature: ["Fast", "Convenient", "Golang"],
   },
 ];
--- froggy-gaming/src/HomepageLayout/homepageMostBuy/components/MostBuyProducts.js
+++ froggy-gaming/src/HomepageLayout/homepageMostBuy/components/MostBuyProducts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { MostBuyData } from "./MostBuyData";
+import "../assets/relevance.css";
 
 const MostBuyProducts = () => {
   return (
@@ -7,11 +8,32 @@ const MostBuyProducts = () => {
       {MostBuyData.length > 0 &&
         MostBuyData.map((item, index) => (
           <div className="relevance-item" key={item.id}>
+            {/* 2 cái bên dưới align dọc với nhau, đều được bọc bởi 1 div và div đó có background skew */}
             <div className="relevance-img">
               <img src={item.img_url} alt="" />
             </div>
+            {/* Background skew */}
             <div className="relevance-description">
               <div className="relevance-name">{item.product_name}</div>
+              <div className="relevance-compatible">
+                {/* Hotswap, Bluetooth, Wireless, ... */}
+                {MostBuyData.map((item) => (
+                  <div className="relevance-compatible-item" key={item.id}>
+                    {item.product_compatible.map((item, index) => (
+                      <img src={item} alt="k" key={index} />
+                    ))}
+                  </div>
+                ))}
+              </div>
+              {/* Grid */}
+              <div className="relevance-specification">
+                <div className="relevance-brand">{item.brand}</div>
+                <div className="relevance-keycap">{item.keycap}</div>
+                <div className="relevance-switch">{item.switch}</div>
+                <div className="relevance-layout">{item.layout}</div>
+                <div className="relevance-weight">{item.weight}</div>
+                <div className="relevance-feature">{item.feature}</div>
+              </div>
             </div>
           </div>
         ))}
