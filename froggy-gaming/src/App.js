5d233975042c3d14ad924feb5a8c9771df4663ef
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-23 19:03:15 +0700

    :bouquet:: Refactor App.js and add media queries to relevanct product section

--- froggy-gaming/src/App.js
+++ froggy-gaming/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Nav from "./HomepageLayout/navigationBar/components/Nav";
-import SlickSlider from "./HomepageLayout/homepageSlider/components/SlickSlider";
 import HomepageContainer from "./HomepageLayout/homepageContainer/components/HomepageContainer";
 import { useEffect } from "react";
 import Aos from "aos";
@@ -14,7 +13,6 @@ function App() {
   return (
     <div>
       <Nav></Nav>
-      <SlickSlider></SlickSlider>
       <HomepageContainer></HomepageContainer>
       <ContactBar></ContactBar>
       <ScrollToTop></ScrollToTop>
--- froggy-gaming/src/HomepageLayout/homepageContainer/components/HomepageContainer.js
+++ froggy-gaming/src/HomepageLayout/homepageContainer/components/HomepageContainer.js
@@ -1,29 +1,34 @@
 import React from "react";
+import { Fragment } from "react";
 import HomepageCategory from "../../homepageCategory/components/HomepageCategory";
 import HomepageEvent from "../../homepageEvent/components/HomepageEvent";
 import MostBuyProducts from "../../homepageMostBuy/components/MostBuyProducts";
+import SlickSlider from "../../homepageSlider/components/SlickSlider";
 import SectionDivider from "../../sectionDivider/components/SectionDivider";
 import "../assets/homepagecontainer.css";
 
 const HomepageContainer = () => {
   return (
-    <div className="homepage">
-      <SectionDivider
-        sectionContent={"Chương trình và sự kiện"}
-        marginBlock={"1.5rem"}
-      ></SectionDivider>
-      <HomepageEvent></HomepageEvent>
-      <SectionDivider
-        sectionContent={"Danh mục sản phẩm"}
-        marginBlock={"3.5rem"}
-      ></SectionDivider>
-      <HomepageCategory></HomepageCategory>
-      <SectionDivider
-        sectionContent={"Sản phẩm nổi bật"}
-        marginBlock={"5rem"}
-      ></SectionDivider>
-      <MostBuyProducts></MostBuyProducts>
-    </div>
+    <Fragment>
+      <SlickSlider></SlickSlider>
+      <div className="homepage">
+        <SectionDivider
+          sectionContent={"Chương trình và sự kiện"}
+          marginBlock={"1.5rem"}
+        ></SectionDivider>
+        <HomepageEvent></HomepageEvent>
+        <SectionDivider
+          sectionContent={"Danh mục sản phẩm"}
+          marginBlock={"3.5rem"}
+        ></SectionDivider>
+        <HomepageCategory></HomepageCategory>
+        <SectionDivider
+          sectionContent={"Sản phẩm nổi bật"}
+          marginBlock={"5rem"}
+        ></SectionDivider>
+        <MostBuyProducts></MostBuyProducts>
+      </div>
+    </Fragment>
   );
 };
 
