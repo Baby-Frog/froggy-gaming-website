a29a88b44441b5a1dfeb7b4c7e8a400ed183b3a5
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-26 16:42:37 +0700

    :+1:: Finish footer and added responsive to footer

--- froggy-gaming/src/global/footer/Footer.js
+++ froggy-gaming/src/global/footer/Footer.js
@@ -1,39 +1,59 @@
 import React from "react";
-import Logo from "../navigationBar/assets/froggy-gaming-logo.png";
+import Badge from "../../assets/badge.png";
 import "../footer/styles/footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <FooterContent
-          header={"Thông tin chung"}
-          row1="Giới thiệu Froggy Gaming"
-          row2="Tin tức"
-          row3="Đánh giá khách hàng"
-          row4="Liên hệ"
-        ></FooterContent>
-        <FooterContent
-          header={"Chính sách chung"}
-          row1="Quy định chung"
-          row2="Chính sách vận chuyển"
-          row3="Chính sách bảo hành"
-          row4="Chính sách đổi, trả lại hàng"
-          row5="Chính sách cho doanh nghiệp"
-        ></FooterContent>
-        <FooterContent
-          header={"Thông tin khuyến mãi"}
-          row1="Sản phẩm bán chạy"
-          row2="Sản phẩm khuyến mãi"
-          row3="Hàng thanh lý"
-        ></FooterContent>
-        <FooterContent
-          header={"Follow us! <3"}
-          row1={<i class="fa-brands fa-facebook"></i>}
-          row2={<i class="fa-brands fa-youtube"></i>}
-          row3={<i class="fa-brands fa-tiktok"></i>}
-          row4={<i class="fa-brands fa-instagram"></i>}
-        ></FooterContent>
+        <div className="footer-list">
+          <FooterContent
+            header={"Thông tin chung"}
+            row1="Giới thiệu Froggy Gaming"
+            row2="Tin tức"
+            row3="Đánh giá khách hàng"
+            row4="Liên hệ"
+          ></FooterContent>
+          <FooterContent
+            header={"Chính sách chung"}
+            row1="Quy định chung"
+            row2="Chính sách vận chuyển"
+            row3="Chính sách bảo hành"
+            row4="Chính sách đổi, trả lại hàng"
+            row5="Chính sách cho doanh nghiệp"
+          ></FooterContent>
+          <FooterContent
+            header={"Thông tin khuyến mãi"}
+            row1="Sản phẩm bán chạy"
+            row2="Sản phẩm khuyến mãi"
+            row3="Hàng thanh lý"
+          ></FooterContent>
+          <FooterContent
+            header={"Follow us! <3"}
+            row1={<i class="fa-brands fa-facebook"></i>}
+            row2={<i class="fa-brands fa-youtube"></i>}
+            row3={<i class="fa-brands fa-tiktok"></i>}
+            row4={<i class="fa-brands fa-instagram"></i>}
+          ></FooterContent>
+        </div>
+        <div className="footer-storeinfo">
+          <div className="footer-storeinfo-left">
+            <h3>Công ty TNHH Thương Mại Froggy Gaming</h3>
+            <div className="footer-address">
+              <p>
+                Showroom HN: 18A Tôn Thất Thuyết, Mỹ Đình, Nam Từ Liêm, Hà Nội /
+                Tel: (096) 9277386 / (086) 830.2123
+              </p>
+              <p>
+                Trung tâm bảo hành : 8A Tôn Thất Thuyết, Mỹ Đình, Nam Từ Liêm,
+                Hà Nội / Tel: (096) 9277386 / (086) 830.2123
+              </p>
+            </div>
+          </div>
+          <div className="footer-storeinfo-right">
+            <img src={Badge} alt="Badge" />
+          </div>
+        </div>
       </div>
     </footer>
   );
